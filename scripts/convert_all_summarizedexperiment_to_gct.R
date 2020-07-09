15bee392dd435fc557a4537495ea90c8886c18b2
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2020-07-09 10:40:10 -0500

    Parallelize SummarizedExperiment to GCT conversion

--- scripts/convert_all_summarizedexperiment_to_gct.R
+++ scripts/convert_all_summarizedexperiment_to_gct.R
@@ -1,21 +1,24 @@
+library(future.apply)
+plan(multicore(workers = 6L))  # parallel cores
+
 library(SummarizedExperiment)
 library(cmapR)
 library(here)
-library(tidyverse)
+library(fs)
+library(dplyr)
 
 all_se_obj_pths = fs::dir_ls(here('final_output/'), glob = '*.rds', recurse = TRUE)
 
-all_se_obj_pths %>%
-    walk(function(pth){
-        se = readRDS(pth)
-        out_pth = fs::path_ext_set(pth, 'gct')
-        g = cmapR::GCT(
-            mat = assay(se),
-            rdesc = rowData(se) %>% as.data.frame(),
-            cdesc = colData(se) %>% as.data.frame()
-        )
-        cmapR::write_gct(g, out_pth, precision = 6, appenddim = FALSE)
-    })
+# Read SummarizedExperiment object from pth and export as GCT
+se_to_gct = function(pth) {
+    se = readRDS(pth)
+    out_pth = fs::path_ext_set(pth, 'gct')
+    g = cmapR::GCT(
+        mat = assay(se),
+        rdesc = rowData(se) %>% as.data.frame(),
+        cdesc = colData(se) %>% as.data.frame()
+    )
+    cmapR::write_gct(g, out_pth, precision = 6, appenddim = FALSE)
+}
 
-
-
+y = future_lapply(all_se_obj_pths, se_to_gct)
